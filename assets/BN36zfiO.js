2f1ba8e7b449cf691f4120ea167240e3feac94b6
qRnt <qRnt@yandex.com>
2024-07-08 19:14:50 +0300

    Updates

---
+++ assets/BN36zfiO.js
@@ -0,0 +1,1 @@
+import{_ as t,v as a,B as o,z as n,L as s,i as c,t as r}from"./B2O6GIPv.js";const _={};function l(m,f){const e=c("ChangePage");return r(),a(s,null,[o(" this is about "),n(e)],64)}const p=t(_,[["render",l]]);export{p as default};
--- assets/LDbDi9vF.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,v as a,B as o,z as n,L as s,i as c,t as r}from"./DASkxCSE.js";const _={};function l(m,f){const e=c("ChangePage");return r(),a(s,null,[o(" this is about "),n(e)],64)}const p=t(_,[["render",l]]);export{p as default};
